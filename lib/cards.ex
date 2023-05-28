035eeb8e366f807e580504ea8cde4cf4c613460a
Emmanuel Sanchez <fede@fede.website>
2023-05-27 20:33:26 -0600

    multiple functions on one

--- lib/cards.ex
+++ lib/cards.ex
@@ -36,4 +36,11 @@ defmodule Cards do
     end
 
   end
+
+  def create_hand(hand_size) do
+    deck = Cards.create_deck
+    deck = Cards.shuffle(deck)
+    hand = Cards.deal(deck, hand_size)
+  end
+
 end
